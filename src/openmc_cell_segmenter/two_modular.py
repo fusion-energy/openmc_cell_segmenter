25e3a7d73592d7ed6cbe6e0b5728aeaadf297e9c
Jonathan Shimwell <mail@jshimwell.com>
2023-09-14 10:34:28 +0100

    surfaces seperatly made

--- src/openmc_cell_segmenter/two_modular.py
+++ src/openmc_cell_segmenter/two_modular.py
@@ -5,47 +5,51 @@ import matplotlib.pyplot as plt
 
 def segment_cell_with_surfaces(cell, number_of_segments, surface_types=['zcylinder']):
 
-    for surface_type in surface_types:
-        print('segmenting with', surface_type)
+    print('segmenting with', surface_types[0])
+
+    surfaces = get_surfaces_for_segmentation(
+        cell,
+        number_of_segments[0],
+        surface_type=surface_types[0]
+    )
+
     first_segment_of_cells = segment_cell_with_surface(
         cell,
-        number_of_segments,
-        surface_type=surface_types[0]
+        surfaces
+    )
+
+    print('segmenting with', surface_types[1])
+
+    surfaces = get_surfaces_for_segmentation(
+        cell,
+        number_of_segments[1],
+        surface_type=surface_types[1]
     )
 
     all_new_cells = []
     for once_segmented_cell in first_segment_of_cells:
         new_cells = segment_cell_with_surface(
-            once_segmented_cell,
-            number_of_segments,
-            surface_type=surface_types[1],
-            bounding_box=cell.bounding_box
+            once_segmented_cell, surfaces
         )
         all_new_cells = all_new_cells + new_cells
 
 
     return all_new_cells
 
+def get_surfaces_for_segmentation(cell, number_of_segments, surface_type):
 
-def segment_cell_with_surface(
-        cell,
-        number_of_segments,
-        surface_type='zcylinder',
-        bounding_box=None
-    ):
+    # if bounding_box==None:
+    min_x = cell.bounding_box[0][0]
+    max_x = cell.bounding_box[1][0]
 
-    if bounding_box==None:
-        min_x = cell.bounding_box[0][0]
-        max_x = cell.bounding_box[1][0]
+    min_z = cell.bounding_box[0][0]
+    max_z = cell.bounding_box[1][0]
+    # elif bounding_box:
+        # min_x = bounding_box[0][0]
+        # max_x = bounding_box[1][0]
 
-        min_z = cell.bounding_box[0][0]
-        max_z = cell.bounding_box[1][0]
-    elif bounding_box:
-        min_x = bounding_box[0][0]
-        max_x = bounding_box[1][0]
-
-        min_z = bounding_box[0][0]
-        max_z = bounding_box[1][0]
+        # min_z = bounding_box[0][0]
+        # max_z = bounding_box[1][0]
 
     # print(min_x, max_x)
     x_width_of_segment = abs(min_x-max_x)/number_of_segments
@@ -56,7 +60,10 @@ def segment_cell_with_surface(
         x_width_of_segment = max_x/number_of_segments
         z_width_of_segment = max_z/number_of_segments
         # todo check if max_y is larger than max_x
-        radiuses = np.linspace(x_width_of_segment/2, max_x, number_of_segments, endpoint=False)
+
+        # inner surface is double of the width
+        # 0-----5----------10----------20
+        radiuses = np.linspace(x_width_of_segment/2, max_x, number_of_segments-1, endpoint=False)
         # print('segmentation', radiuses)
     if surface_type == 'zplane':
         radiuses = np.linspace(min_z+z_width_of_segment, max_z, number_of_segments, endpoint=False)
@@ -68,6 +75,15 @@ def segment_cell_with_surface(
             surfaces.append(openmc.ZCylinder(r=radius))
         if surface_type == 'zplane':
             surfaces.append(openmc.ZPlane(z0=radius))
+    
+    return surfaces
+
+
+def segment_cell_with_surface(
+        cell,
+        surfaces
+    ):
+
 
     new_regions = []
     for i, surface in enumerate(surfaces):
@@ -106,7 +122,7 @@ blanket_region = -inner_sphere_surface
 # example cell
 cell = openmc.Cell(region=blanket_region)
 
-cells = segment_cell_with_surfaces(cell, 5, ['zcylinder', 'zplane'])
+cells = segment_cell_with_surfaces(cell, [2,5], ['zcylinder', 'zplane'])
 
 geometry = openmc.Geometry(cells)  
 
